5a022d2742db761865d747328e6d8f7ae6a92b53
drewholliday <165310098+drewholliday@users.noreply.github.com>
2024-05-22 21:19:12 -0500

    worked on local storage logic

---
+++ assets/js/form.js
@@ -0,0 +1,23 @@
+document.getElementById("blogForm").addEventListener('submit',function(event) {
+    event.preventDefault();
+
+    const username = document.getElementById('username').value;
+    const title = document.getElementById('title').value;
+    const content = document.getElementById('content').value;
+
+
+    const post = {
+        username,
+        title,
+        content
+    };
+
+
+    let posts = JSON.parse(localStorage.getItem(posts)) || [];
+    posts.push(post);
+    localStorage.setItem('posts', JSON.stringify(post));
+
+    window.location.href = 'blog.html'
+
+
+})
